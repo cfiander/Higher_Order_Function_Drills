09b12f4a48831a1f757c3cc06d2c28e8872a7e15
cfiander <cfiander90@gmail.com>
2019-04-30 11:43:17 -0700

    hazard warning

--- app.js
+++ app.js
@@ -78,3 +78,21 @@ const filteredNames = filter(myNames, function(name) {
 
 console.log(filteredNames) // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
+
+const hazardWarningCreator = function(typeOfWarning) {
+    let warningCounter = 0;
+    return function(location) {
+        warningCounter ++;
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+    }
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const avalancheWarning = hazardWarningCreator('There is an avalanche on the mountain');
+const tornadoWarning = hazardWarningCreator('Tornado on the field');
+
+
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+tornadoWarning('Oklahoma');
